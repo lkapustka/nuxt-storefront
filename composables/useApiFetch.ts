bda5a1f6a0a09d7485c99989d0c816ad58da87c1
Łukasz Kapustka <lukaszkapustka@gmail.com>
2023-07-16 01:30:52 +0200

    feat: set default key value in useApiFetch

--- composables/useApiFetch.ts
+++ composables/useApiFetch.ts
@@ -3,9 +3,7 @@ type useFetchType = typeof useFetch;
 export const useApiFetch: useFetchType = (path, options = {}) => {
   const config = useRuntimeConfig();
 
-  if (!options.key) {
-    options.key = path as string;
-  }
+  options.key = options.key ?? (path as string);
   options.baseURL = config.public.apiBaseURL;
 
   return useFetch(path, options);
