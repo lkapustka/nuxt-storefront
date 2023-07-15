875e34a039ea6d372b81ecc3c491dee8dfbd63cd
Łukasz Kapustka <lukaszkapustka@gmail.com>
2023-07-16 00:31:52 +0200

    test: add key option test for useApiFetch

--- composables/useApiFetch.ts
+++ composables/useApiFetch.ts
@@ -3,6 +3,10 @@ type useFetchType = typeof useFetch;
 export const useApiFetch: useFetchType = (path, options = {}) => {
   const config = useRuntimeConfig();
 
+  if (!options.key) {
+    options.key = path as string;
+  }
   options.baseURL = config.public.apiBaseURL;
+
   return useFetch(path, options);
 };
